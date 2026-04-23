46049c212be89625bedefe78863369ed18e3fb8b
Yasmin Bomfim <yasmimlima123@gmail.com>
2026-04-23 19:04:04 -0300

    corrigindo import

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,10 @@
+import sys
+import os
+
 from fastapi.testclient import TestClient
+
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
 from main import app
 
 client = TestClient(app)
